84e515137989d2dbce8367022e9a2a9ba6a31b5b
N. McCallum <ilovehockey95@hotmail.com>
2015-01-14 16:36:25 -0500

    Fixed command addition to reload module.

--- commands.py
+++ commands.py
@@ -1,12 +1,13 @@
 """
     Command file used to store bot commands and search the commands for the appropriate response.
         @author: N. McCallum
-        @version: 1
-        @date: 12/01/2015
+        @version: 2
+        @date: 14/01/2015
 """
 
 # Imports
 import config as settings
+import importlib
 
 
 class Command(object):
@@ -17,18 +18,26 @@ class Command(object):
 
     def checkForMatch(self):
         # Check for a match in the array and return true if it is there
-        for k, v in self.matches:
+        for k in self.matches:
             if k in self.data:
                 return True
 
         return False
 
     def findCommand(self):
+        # Reload the command file to check for new commands
+        importlib.reload(settings)
+        self.matches = settings.config['commands']
+
+        # Debug information
+        print("[DEBUG] Commands:")
+        print(self.matches)
+
         # Check for the match and if it is there return the value that goes with the command
         if self.checkForMatch():
-            for k, v in self.matches:
+            for k in self.matches:
                 if k in self.data:
-                    return v
+                    return self.matches.get(k)
         else:
             return None
 
@@ -42,8 +51,8 @@ class Command(object):
         print(data)
 
         for i in range(0, len(data)):
-            if "# End of commands" in data[i]:
-                data.insert(i - 2, com)
+            if "# Custom commands added below" in data[i]:
+                data.insert(i + 1, com + "\n")
 
         # Debug information
         print("[DEBUG] Printing data contents after command insertion:")
@@ -51,6 +60,6 @@ class Command(object):
 
         print("Attempting to add command...")
         with open('config.py', 'w') as file:
-            file.write(data)
+            file.write("".join(data))
 
         print("Command has been added.")
