e8da7edce4013960fa948c3266fc0551fb012cf2
N. McCallum <ilovehockey95@hotmail.com>
2015-01-14 16:38:02 -0500

    Added the add command portion.

--- bot.py
+++ bot.py
@@ -2,13 +2,14 @@
     Bot file to set-up and run bot in chat. Scans chat for commands and replies with
     the value set in the config file.
         @author: N. McCallum
-        @version: 1
-        @date: 13/01/2015
+        @version: 2
+        @date: 14/01/2015
 """
 
 import _socket
 import config as settings   #Change folder location to config.config
 import commands
+import re
 
 
 class Bot(object):
@@ -20,9 +21,9 @@ class Bot(object):
     #     Output:
     #         None.
     #=======================================================
-    def __init__(self, channel):
+    def __init__(self):
         # Set the channel to the user input
-        self.channel = channel
+        self.channel = settings.config['channel']
 
         # Create new socket to connect to irc
         self.s = _socket.socket()
@@ -72,14 +73,21 @@ class Bot(object):
         # Receive data and add to the buffer
         data = self.s.recv(bufferSize).decode()
         data = data.rstrip()
-        self.readBuffer += data
+        self.readBuffer += data + "\n"
 
         # Print the buffer to the screen
-        print("[DEBUG] Cat'ing Buffer to StdOut:")
+        print("[DEBUG] Cat'ing Buffer to StdOut:")  # FIXME: Not sure if printing buffer to screen each time is needed
         print(self.readBuffer)
 
         return data
 
+    #===========================================================================================
+    # Main function with loop to run the bot continuously on irc server.
+    #     Input:
+    #         None.
+    #     Output:
+    #         None.
+    #===========================================================================================
     def run(self):
         # Loop while connected to chat
         while True:
@@ -94,27 +102,64 @@ class Bot(object):
 
             # Check for ping and reply with pong
             if buffer[:4] == "PING":
+                print("[DEBUG] Ping command detected. Sending pong...")
                 self.s.send("PONG".encode())
 
-            # Check for a message
-            if "!test" in buffer:
-                self.s.send(("PRIVMSG %s :Test command received.\r\n" % self.channel).encode())
-
+            # Check if streamer gave disconnect command and disconnect from chat
             if "justpwnedu!justpwnedu@justpwnedu.tmi.twitch.tv PRIVMSG #justpwnedu :!disconnect" in buffer:
                 self.s.send(("PRIVMSG %s :JPUBot Disconnecting from channel.\r\n" % self.channel).encode())
                 self.s.send("QUIT \r\n".encode())
                 exit(0)
 
+            # Check for a command in the buffer and print the appropriate response
+            c = commands.Command(buffer)
+            com = c.findCommand()
+            if com is not None:
+                print("[DEBUG] Printing Command: %s" % com)
+                # Check if com requires a function call
+                if com == 'addcom':
+                    # Debug information
+                    print("[DEBUG] ADDCOM DETECTED")
+                    print("[DEBUG] Printing buffer: %s" % buffer)
+
+                    # Isolate the command to add into the file
+                    words = re.sub('^.*:!addcom', '', buffer).strip()
+
+                    # Split the words into the command and the value
+                    words = words.split(" ")
+                    newCommand = words[0]
+                    newValue = words[1:len(words)]
+                    newValue = " ".join(newValue)
+
+                    # FIXME: Add default ! to command
+                    # Send the string to add to function
+                    c.addCom("        , '%s': '%s'" % (newCommand, newValue))
+                else:   # Doesn't require function call so print command
+                    self.s.send(("PRIVMSG %s :%s\r\n" % (self.channel, com)).encode())
+
             data = buffer.split("\n")
             if data[0] == "PING":
                 self.s.send("PONG".encode())
 
+    #===========================================================================================
+    # Function to isolate user from buffer in order to find who put command into chat on
+    # irc server.
+    #     Input:
+    #         data - Buffer that the function should isolate user on.
+    #     Output:
+    #         None.
+    #===========================================================================================
+    def determineUser(self, data):
+        # Print for debug info and return the value of the user
+        print(re.sub('!.*$', '', data.strip().split(" ")[0].strip(":")))
+        return re.sub('!.*$', '', data.strip().split(" ")[0].strip(":"))
 
-    #def checkForCommand(self, data):
-        #FIXME: delegate later to commands.py
+    #def say(self, msg):
+        # FIXME: implement function to write to channel
+
 
 def main():
-    ircBot = Bot("#justpwnedu")
+    ircBot = Bot()
     ircBot.connect()
     ircBot.run()
 
